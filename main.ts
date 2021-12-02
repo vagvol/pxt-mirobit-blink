b4c977184631f0bb58859a7b83bd15edd11a5492
vagvol <46868546+vagvol@users.noreply.github.com>
2021-12-02 10:55:03 +0200

    Ενημέρωση pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+/**
+* Cool LED functionalities
+*/
+namespace ledTricks {
+    /**
+    * Blinks the LED once.
+    */
+    //% block
+    export function blink() {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pause(500)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pause(500)
+    }
+}
