1d6201ecc155d559be1bf929d244d7e1c1fe9b47
vagvol <46868546+vagvol@users.noreply.github.com>
2021-12-02 11:11:06 +0200

    Ενημέρωση main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
-/**
-* Cool LED functionalities
-*/
 namespace ledTricks {
     /**
     * Blinks the LED once.
     */
     //% block
     export function blink() {
-        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
         pause(500)
-        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
         pause(500)
     }
 }
